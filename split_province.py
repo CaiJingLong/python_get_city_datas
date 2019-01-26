86890372bcdbbb24890f1be0d894a2ffc1d847f5
cjl_macbook <cjl_spy@163.com>
2019-01-26 10:47:21 +0800

    给拆分的省份数据中增加了编号,版本号,时间戳和生成时间

--- split_province.py
+++ split_province.py
@@ -1,6 +1,10 @@
 import json
 
-f = open('./data/city-version-3.json')
+from city.version_util import get_version
+
+version = get_version()
+
+f = open('./data/city-version-%s.json' % version)
 
 obj: dict = json.load(f)
 
@@ -9,4 +13,7 @@ province_list: list = obj["provinceList"]
 for province in province_list:
     # city_list = province["cityList"]
     province_file = open('./province_data/%s.json' % province["name"], 'w')
+    province["version"] = obj["version"]
+    province["date"] = obj["date"]
+    province["timeStamp"] = obj["timeStamp"]
     json.dump(province, province_file, indent="  ")
